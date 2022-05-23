a2eb83c09a9d5d665674bbd34fbcca0cda4dd42c
Pvan <pawankumarp2000@outlook.com>
2022-05-23 01:48:16 -0500

    Update streamlit_app.py
    add a new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,10 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+
 
 
 

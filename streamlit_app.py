b09ec606e0faa94ad5e312982b3850acc4ed3503
Pvan <pawankumarp2000@outlook.com>
2022-05-25 00:14:26 -0500

    Update streamlit_app.py
    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice) 
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #take the json version of the response and normalize it
   return fruityvice_normalized
   
@@ -41,7 +41,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
-    back_from_function = get_fruityvice_data(this_fruit_choice)
+    back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function) #output it the screen as a table
 except URLError as e:
   streamlit.error()
